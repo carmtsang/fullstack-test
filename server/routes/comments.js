e41d8ee9b7dd5bbb5bd8cb3944c55dfe3ef60c12
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-21 16:15:23 -0700

    route for getting all comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const { reset } = require("nodemon");
 const router = express.Router();
+const Comment = require("../models/comment");
 
 // getting comments
-router.get("/", (req, res) => {
-  res.send("test??");
+router.get("/", async (req, res) => {
+  try {
+    const comments = await Comment.find();
+    res.json(comments);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // posting comment
