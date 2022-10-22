c09ca245fce11f47ab5e828cd0b5b3cd8e1ce354
carmtsang <carmen.tsang1208@hotmail.com>
2022-10-21 23:41:29 -0700

    create button component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import CommentsList from "./components/CommentsList";
+import Button from "./components/Button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello!</h1>
+        <Button onClick={() => console.log("hello")}> Add Comment</Button>
         <CommentsList />
       </header>
     </div>
